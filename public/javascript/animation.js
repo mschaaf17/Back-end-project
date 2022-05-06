08c015107c45ded7dd84aa290a1ef313d3a8d8bf
Maddy <madisonschaaf48@gmail.com>
2022-05-06 14:03:40 -0600

    erased extra code

--- public/javascript/animation.js
+++ public/javascript/animation.js
@@ -1,9 +1,11 @@
-const animationWelcome = document.querySelector(".animation-welcome")
-const animationInfo = document.querySelector(".animation-info")
+const animationWelcome = document.querySelector(".animation-welcome");
+const animationInfo = document.querySelector(".animation-info");
 
-const tl = new TimelineMax()
+const tl = new TimelineMax();
 
-// tl.fromTo(hero,1,{height: "0%"}, {height: "80%"})
-
-tl.fromTo(animationWelcome, 1.5, {opacity: 0, x:30}, {opacity: 1, x:0})
-.fromTo(animationInfo, 0.5, {opacity: 0, x:30}, {opacity: 1, x:0})
+tl.fromTo(
+  animationWelcome,
+  1.5,
+  { opacity: 0, x: 30 },
+  { opacity: 1, x: 0 }
+).fromTo(animationInfo, 0.5, { opacity: 0, x: 30 }, { opacity: 1, x: 0 });
